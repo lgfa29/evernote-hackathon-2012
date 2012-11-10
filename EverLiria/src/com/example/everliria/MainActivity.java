b4ff77c1bc74a03a0c62951f1ef92893da588935
Marylia Gutierrez <maryliag@gmail.com>
2012-11-10 21:35:39 -0200

    commit parcial

--- EverLiria/src/com/example/everliria/MainActivity.java
+++ EverLiria/src/com/example/everliria/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.everliria;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
+	// culpa da mayra
 	// Names of Evernote-specific Intent actions and extras
 	public static final String ACTION_NEW_NOTE             = "com.evernote.action.CREATE_NEW_NOTE";
 	
@@ -18,16 +22,25 @@ public class MainActivity extends Activity {
 	EditText inputName;
 	Button startListening;
 	Button stopListening;
+	ListView musicList;
 	
 	MusicRecognizer mR = new MusicRecognizer();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+    			  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+    			  "Linux", "OS/2" };
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inputName = (EditText)findViewById(R.id.input_list_name);
         startListening = (Button)findViewById(R.id.start);
         stopListening = (Button)findViewById(R.id.stop);
+        musicList = (ListView)findViewById(R.id.music_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        		  android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        musicList.setAdapter(adapter);
     }
 
     @Override
@@ -36,10 +49,28 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void start(View view) {
+    	mR.startListening();
+    	startListening.setVisibility(View.GONE);
+    	stopListening.setVisibility(View.VISIBLE);
+    	
+//    	ImageView img = (ImageView)findViewById(R.id.elephant_loading);
+//    	img.setBackgroundResource(R.drawable.ever_loading);
+//
+//    	// Get the background, which has been compiled to an AnimationDrawable object.
+//    	AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+//
+//    	// Start the animation (looped playback by default).
+//    	frameAnimation.start();
+    }
+    
     /**
      * Bring up an empty "New Not" activity in Evernote for Android.
      */
     public void newList(View view) {
+    	mR.stopListening();
+    	startListening.setVisibility(View.VISIBLE);
+    	stopListening.setVisibility(View.GONE);
     	Intent intent = new Intent();
     	intent.setAction(ACTION_NEW_NOTE);
     	intent.putExtra(Intent.EXTRA_TITLE, inputName.getText().toString());
