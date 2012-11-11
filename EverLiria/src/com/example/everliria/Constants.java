05ab015421e9c3199ed3b5585199b7e6c9465fa9
Luiz Aoqui <lgfa29@gmail.com>
2012-11-11 04:20:23 -0200

    evita a adição de músicas repetidas na lista

--- EverLiria/src/com/example/everliria/Constants.java
+++ EverLiria/src/com/example/everliria/Constants.java
@@ -4,7 +4,8 @@ public class Constants {
 	public static final String LOG_TAG = "EverLiria";
 	
 	public static final String INTENT_FILTER_TAG = "musicRecognizer";
-	public static final String INTENT_ACTION_MUSIC_FOUND = "musicFound";
+	public static final String INTENT_ACTION_MUSIC_NAME = "musicName";
+	public static final String INTENT_ACTION_MUSIC_ID = "musicId";
 	public static final String INTENT_ACTION_MUSIC_NOT_FOUND = "musicNotFound";
 	public static final String INTENT_ACTION_ERROR = "error";
 }
